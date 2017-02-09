fdb8cdfa5864b42ffd0213f60dbbc6fba2ac50f1
maximzakharov <zakharov.m89@gmail.com>
2017-02-10 01:07:32 +0400

    init commit

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+ *  main.c
+ *
+ *  Created on: 09-02-2017
+ *  Author: Maxim Zakharov
+ *  email:  zakharov.m89@gmail.com
+ */
+#include <stdio.h>
+
+int main(void) {
+
+	printf("Hello world!!!\n");
+
+	while (1);
+	return 0;
+}
