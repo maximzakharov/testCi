6472a9bd00e1f9f8e86c5168e940b6b409a73904
maximzakharov <zakharov.m89@gmail.com>
2017-02-20 22:24:47 +0400

    test Ci

--- main.c
+++ main.c
@@ -5,12 +5,76 @@
  *  Author: Maxim Zakharov
  *  email:  zakharov.m89@gmail.com
  */
+#include <CppUTest/UtestMacros.h>
+#include <CppUTest/CommandLineTestRunner.h>
 #include <stdio.h>
+//#include "CppUTest/TestHarness.h"
+//#include "CppUTest/CommandLineTestRunner.h"
 
+TEST_GROUP(FirstTestGroup)
+{
+
+	void mmm(void);
+};
+
+TEST(FirstTestGroup, FirstTest)
+{
+   FAIL("Fail me!");
+}
+//
+//
+//
 int main(void) {
 
-	printf("Hello world!!!\n")
+	printf("Hello world!!!\n");
 
 	while (1);
 	return 0;
 }
+
+//#include "CppUTest/TestHarness.h"
+//#include "ClassName.h"
+
+//SimpleString StringFrom(const yourType&);
+//
+//
+//
+//TEST_GROUP(ClassName)
+//{
+//  ClassName* className;
+//
+//  void setup()
+//  {
+//    className = new ClassName();
+//  }
+//  void teardown()
+//  {
+//    delete className;
+//  }
+//}
+//
+//TEST(ClassName, Create)
+//{
+//  CHECK(0 != className);
+//  CHECK(true);
+//  CHECK_EQUAL(1,1);
+//  LONGS_EQUAL(1,1);
+//  DOUBLES_EQUAL(1.000, 1.001, .01);
+//  STRCMP_EQUAL("hello", "hello");
+//  FAIL("The prior tests pass, but this one doesn't");
+//}
+
+
+//
+//int main(int ac, char** av)
+//{
+//   LogPlugin logPlugin;
+//   TestRegistry::getCurrentRegistry()->installPlugin(&logPlugin);
+//   int result = CommandLineTestRunner::RunAllTests(ac, av);
+//   TestRegistry::getCurrentRegistry()->resetPlugins();
+//   return result;
+//}
+
+
+
+
--- makefile
+++ makefile
@@ -4,6 +4,8 @@
 
 -include ../makefile.init
 
+CPPUTEST_HOME = ../cpputest
+
 RM := rm -rf
 
 # All of the sources participating in the build are defined here
@@ -17,6 +19,11 @@ ifneq ($(strip $(C_DEPS)),)
 endif
 endif
 
+INC_FOLDERS += \
+  $(CPPUTEST_HOME)/include
+
+LDFLAGS += -L$(CPPUTEST_HOME)/lib -lCppUTest -lCppUTestExt
+
 -include ../makefile.defs
 
 # Add inputs and outputs from these tool invocations to the build variables 
@@ -27,8 +34,8 @@ all: testCi
 # Tool invocations
 testCi: $(OBJS) $(USER_OBJS)
 	@echo 'Building target: $@'
-	@echo 'Invoking: GCC C Linker'
-	gcc  -o "testCi" $(OBJS) $(USER_OBJS) $(LIBS)
+	@echo 'Invoking: G++ C Linker'
+	g++  -o "testCi" $(OBJS) $(USER_OBJS) $(LIBS)
 	@echo 'Finished building target: $@'
 	@echo ' '
 
